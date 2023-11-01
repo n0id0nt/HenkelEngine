ae2054dd843e78d8031ee453483eae6a3214b0f1
n0id0nt <h4bert44@gmail.com>
2023-11-01 22:56:36 +1100

    Started creating my own registry for the ECS

--- HenkelEngine/scr/ECS/Registry.cpp
+++ HenkelEngine/scr/ECS/Registry.cpp
@@ -1,1 +1,12 @@
 #include "Registry.h"
+
+Registry::Registry() : m_components(), m_currentEntityId(0u)
+{
+}
+
+EntityId Registry::CreateEntity()
+{
+	EntityId newEntityId = m_currentEntityId;
+	m_currentEntityId++;
+	return newEntityId;
+}
